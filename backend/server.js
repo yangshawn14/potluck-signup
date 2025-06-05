1f00c2554a6188aaf8dd14367237dca461623ef4
Shawn Yang <yangshawn14@gmail.com>
2025-06-05 02:47:27 -0700

    Comment out wildcard route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 // backend/server.js
 const express = require('express');
+const path = require('path'); // <-- Make sure this is here
 const cors = require('cors');
 const connectDB = require('./db');
 const Dish = require('./models/Dish');
-const path = require('path'); // <-- Make sure this is here
 require('dotenv').config();
 
 const app = express();
@@ -67,8 +67,8 @@ app.put('/dishes/:id', async (req, res) => {
 app.use(express.static(path.join(__dirname, '../public')));
 
 // === Wildcard route (must come LAST) ===
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
